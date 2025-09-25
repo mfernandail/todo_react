a2c3154877b6797cebc9934293b1e2bcc8b2e72d
mfernandail <maria.ibanez.leiva@gmail.com>
2025-09-24 22:08:40 -0300

    Add Footer to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import Footer from './components/Footer'
 import Header from './components/Header'
 import './styles/App.css'
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Header />
-    </>
+
+      <main>
+        <h1>Hola</h1>
+      </main>
+
+      <Footer />
+    </div>
   )
 }
 
