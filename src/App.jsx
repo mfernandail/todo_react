787f03246712ea40c5f9e7e68586016835fa50d7
mfernandail <maria.ibanez.leiva@gmail.com>
2025-09-25 23:02:00 -0300

    Add containet to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import Footer from './components/Footer'
 import Header from './components/Header'
+import TodoContainer from './components/TodoContainer'
 import './styles/App.css'
 
 function App() {
   return (
     <div className="container">
       <Header />
-
-      <main>
-        <h1>Hola</h1>
-      </main>
-
+      <TodoContainer />
       <Footer />
     </div>
   )
