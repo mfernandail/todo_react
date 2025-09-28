e0c6a3fc3998d22f475b0005e1ffc96ee1b607d5
mfernandail <maria.ibanez.leiva@gmail.com>
2025-09-27 22:40:50 -0300

    Implement task validation and error handling in TodoForm component

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,16 +1,45 @@
+import { useState } from 'react'
 import '../styles/TodoForm.css'
 
 function TodoForm() {
+  const [task, setTask] = useState('')
+  const [error, setError] = useState('')
+
+  const handleChange = (e) => {
+    const inputTask = e.target.value
+
+    setTask(inputTask)
+
+    if (inputTask.trim() === '') {
+      setError('Please enter a task')
+    } else {
+      setError('')
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (task.trim() === '') {
+      setError('Please enter a task')
+      return
+    }
+
+    console.log(task)
+  }
+
   return (
     <div className="todo-form">
-      <div className="input-container">
+      <form onSubmit={handleSubmit} className="input-container">
         <input
           type="text"
+          value={task}
+          onChange={handleChange}
           className="todo-input"
           placeholder="What needs to be done?"
         />
         <button className="add-btn">Add</button>
-      </div>
+      </form>
+      {error && <p className="error">{error}</p>}
     </div>
   )
 }
