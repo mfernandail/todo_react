8d204903bb6e3051519c88860d9034bcc0a8dd49
mfernandail <maria.ibanez.leiva@gmail.com>
2025-09-28 21:14:54 -0300

    Implement addTodo function and manage todos state in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import { useState } from 'react'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import TodoContainer from './components/TodoContainer'
 import './styles/App.css'
 
 function App() {
+  const [todos, setTodos] = useState([])
+
+  console.log(todos)
+
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Date.now(),
+      task: todo,
+      complete: false,
+      coreatedAt: new Date(),
+    }
+
+    setTodos((todos) => [...todos, newTodo])
+  }
+
   return (
     <div className="container">
       <Header />
-      <TodoContainer />
+      <TodoContainer addTodo={addTodo} todos={todos} />
       <Footer />
     </div>
   )
