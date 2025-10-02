3751b80c3658fd3b5d89b4b5da2dae3be6684c47
mfernandail <maria.ibanez.leiva@gmail.com>
2025-10-01 22:29:08 -0300

    Add toggle

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,13 +1,18 @@
 import '../styles/TodoList.css'
 
-function TodoList({ todos, deleteTask }) {
+function TodoList({ todos, deleteTask, toggleTodo }) {
   return (
     <>
       <div className="todo-list">
         {todos.map((todo) => {
           return (
             <div key={todo.id} className="todo-item">
-              <input type="checkbox" className="todo-checkbox" />
+              <input
+                type="checkbox"
+                checked={todo.complete}
+                onChange={() => toggleTodo(todo.id)}
+                className="todo-checkbox"
+              />
               <span className="todo-text">{todo.task}</span>
               <button
                 onClick={() => deleteTask(todo.id)}
