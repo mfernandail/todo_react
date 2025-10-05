7311b981c679e6bcb5e664f4bf9c9057128a31e5
mfernandail <maria.ibanez.leiva@gmail.com>
2025-10-05 17:48:47 -0300

    Pass clearComplete and allTodos props to TodoList component

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -5,16 +5,25 @@ import TodoList from './TodoList'
 function TodoContainer({
   addTodo,
   todos,
+  allTodos,
   deleteTask,
   onFilterChange,
   toggleTodo,
   currentFilter,
+  clearComplete,
 }) {
   return (
     <div className="todo-container">
       <TodoForm addTodo={addTodo} />
       <Filters onFilterChange={onFilterChange} currentFilter={currentFilter} />
-      <TodoList todos={todos} deleteTask={deleteTask} toggleTodo={toggleTodo} />
+      <TodoList
+        todos={todos}
+        deleteTask={deleteTask}
+        toggleTodo={toggleTodo}
+        currentFilter={currentFilter}
+        clearComplete={clearComplete}
+        allTodos={allTodos}
+      />
     </div>
   )
 }
